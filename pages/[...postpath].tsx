79b3af30108956cf65080d033469c72e87fcd450
arbiwiratama <37033835+arbiwiratama@users.noreply.github.com>
2023-06-20 21:10:24 +0700

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://bokehmantaphd.blogspot.com/`
+					`https://dooodbokeh.blogspot.com/2023/06/pns.html`
 				}`,
 			},
 		};
